7e6996811fd9f1c26da393bedf38f5e767c58f3c
Annie Góngora <160527392+AnnieyG@users.noreply.github.com>
2025-01-17 20:41:52 +0000

    Se agrego Jumbotron y Navbar a Home

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "./Card";
+import { Navbar } from "./Navbar";
+import { Jumbotron } from "./Jumbotron";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
+			<Navbar/>
+			<Jumbotron/>
 			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
 				<Card />
 				<Card />
