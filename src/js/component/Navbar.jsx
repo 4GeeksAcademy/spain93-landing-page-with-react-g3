036724a452c09dfefb8bc6fbd8fefc438371a90e
chriss-camr <chriss02.camr@gmail.com>
2025-01-17 20:14:40 +0000

    merge develop

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-export const navbar = () => {
+export const Navbar = () => {
     return (<nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
             <a className="navbar-brand" href="#">Navbar</a>
